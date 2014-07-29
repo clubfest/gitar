da25a405fcc038cf4d467086fd80c46cbf54280f
Chor <cl676@cornell.edu>
2014-07-29 12:21:21 -0400

    starting to add drag event

--- client/view/guitar/GuitarDrawer.js
+++ client/view/guitar/GuitarDrawer.js
@@ -5,34 +5,63 @@ GuitarDrawer = {
       
     // TODO: handle resize
 
-    this.paper = new Raphael(0, 0, this.width, this.height);
+    this.paper = new Raphael(0, 10, this.width, this.height);
     this.draw();
     // TODO: interact
   },
 
   computeDimensions: function(){
-    this.width = $(window).width();
-    this.height = $(window).height();
+    this.width = Math.min($(window).width(), 600);
+    this.height = this.width / 2.5;
 
-    this.keyWidth = this.width / 12;
+    this.keyWidth = this.width / 10;
     this.keyHeight = this.height / 4;
   },
 
   draw: function(){
-    this.drawStrings();
     this.drawKeys();
+    this.drawStrings();
   },
 
   drawStrings: function(){
+    for (var i = 0; i < 4; i++) {
+      var command = 'M0 ';
 
+      var y = (i + 0.5) * this.keyHeight;
+      command += y;
+      command += 'L' + this.width + ' ' + y;
+      var path = this.paper.path(command);
+      path.attr({
+        stroke: '#333',
+        'stroke-width': '5',
+      });
+    }
   },
 
   drawKeys: function(){
     for (var i = 0; i < 4; i++){
-      for (var k = 0; k < 12; k++){
+      for (var k = 0; k < 10; k++){
         var x = k * this.keyWidth;
         var y = i * this.keyHeight;
-        this.paper.rect(x, y, this.keyWidth, this.keyHeight);
+        if (k % 2) {
+          var attr = {fill: '#e80'}
+        } else {
+          var attr = {fill: '#e90'}
+        }
+        var key = this.paper
+          .rect(x, y, this.keyWidth, this.keyHeight)
+          .attr(attr)
+          .drag(move, start, up);
+
+        var move = function(){
+
+        }
+        var start = function(){
+          MIDI.noteOn(0, 60, 100);
+        }
+        var up = function(){
+          MIDI.noteOff(0, 60);
+        }
       }
     }
   },
